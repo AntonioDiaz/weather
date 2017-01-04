4d245ec1ed752199926bb1075badaff474ca8eac
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2017-01-04 09:05:51 +0100

    add projections

--- app/src/main/java/com/example/android/sunshine/app/ForecastCursorAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastCursorAdapter.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.android.sunshine.app.data.WeatherContract;
-
 /**
  * {@link ForecastCursorAdapter} exposes a list of weather forecasts
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
@@ -20,39 +18,45 @@ public class ForecastCursorAdapter extends CursorAdapter {
 		super(context, cursor, flags);
 	}
 
-	/** Prepare the weather high/lows for presentation. */
+	/**
+	 * Prepare the weather high/lows for presentation.
+	 */
 	private String formatHighLows(double high, double low) {
 		boolean isMetric = Utility.isMetric(mContext);
 		String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
 		return highLowStr;
 	}
 
-	/** This is ported from FetchWeatherTask --- but now we go straight from the cursor to the string.*/
+	/**
+	 * This is ported from FetchWeatherTask --- but now we go straight from the cursor to the string.
+	 */
 	private String convertCursorRowToUXFormat(Cursor cursor) {
 		// get row indices for our cursor
-		int idxMaxTemp = cursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP);
-		int idxMinTemp = cursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP);
-		int idxDate = cursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_DATE);
-		int idxShortDesc = cursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_SHORT_DESC);
-		String highAndLow = formatHighLows(	cursor.getDouble(idxMaxTemp), cursor.getDouble(idxMinTemp));
-		String uxFormat = Utility.formatDate(cursor.getLong(idxDate));
-		uxFormat += " - " + cursor.getString(idxShortDesc);
-		uxFormat +=	" - " + highAndLow;
+		double maxTemp = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+		double minTemp = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+		String highAndLow = formatHighLows(maxTemp, minTemp);
+		String uxFormat = Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE));
+		uxFormat += " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+		uxFormat += " - " + highAndLow;
 		return uxFormat;
 	}
 
-	/** Remember that these views are reused as needed. */
+	/**
+	 * Remember that these views are reused as needed.
+	 */
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup parent) {
 		return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
 	}
 
-	/** This is where we fill-in the views with the contents of the cursor. */
+	/**
+	 * This is where we fill-in the views with the contents of the cursor.
+	 */
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
 		// our view is pretty simple here --- just a text view
 		// we'll keep the UI functional with a simple (and slow!) binding.
-		TextView tv = (TextView)view;
+		TextView tv = (TextView) view;
 		tv.setText(convertCursorRowToUXFormat(cursor));
 	}
 }
